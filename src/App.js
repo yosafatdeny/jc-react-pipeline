7456236099c9a2c877c7450f32587b9646956f5c
hisbu <lab.hisbu@gmail.com>
2020-11-05 15:15:30 +0700

    add missing file

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          test
         </p>
       </div>
     );
